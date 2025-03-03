a8b71fdf721494891c2f09225414854e2f65af20
F.K.A. Soffers <diorcula@duck.com>
2025-03-03 08:48:04 +0100

    customlinktoview

--- payload-medusa-prototype/src/plugins/medusa-plugin/afterNavLinks/LinkToCustomView.tsx
+++ payload-medusa-prototype/src/plugins/medusa-plugin/afterNavLinks/LinkToCustomView.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 
 export const LinkToCustomView: React.FC = () => {
-  return <Link href="/medusa-plugin">Medusa Products</Link>
+  return <Link href="/admin/medusa-plugin">Medusa Products</Link>
 }
 
 export default LinkToCustomView
